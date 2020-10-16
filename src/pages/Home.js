7ae135650d5368b4b7a70c4973ac39676573d8c9
omirbeck <ommirtull@gmail.com>
2020-10-16 17:45:32 +0600

    feat: add card, input component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import Card from '../components/Card'
+import Search from '../components/Search'
 
 const Home = () => {
+  const cards = new Array(12).fill('');
   return (
-    <div>
-      <h1>Home page</h1>
-    </div>
+    <>
+      <Search />
+      <div className="row">
+        {cards.map((card) => {
+          return (
+            <div className="col-sm-4 mb-4" key={card}>
+          <Card />
+        </div>
+          )
+        })}
+      </div>
+    </>
   )
 }
 
