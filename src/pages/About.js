0eeb26041e74b73bc2803f1d4927c9b496e62899
omirbeck <ommirtull@gmail.com>
2020-10-16 16:00:57 +0600

    feat: add pages and routing

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div class="jumbotron">
+      <div class="container">
+        <h1 class="display-4">Информация</h1>
+        <p class="lead">Вресия приложения: <strong>1.0.0</strong></p>
+      </div>
+    </div>
+  )
+}
+
+export default About
